f9a0a5ece59b832a8729fc71ac251a1163788828
GQ <164480701+yongkyu4803@users.noreply.github.com>
2025-07-09 08:12:50 +0900

    aaa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
+import "bootstrap";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     // Bootstrap JS를 동적으로 로드
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    import("bootstrap").then(() => {
+      // Bootstrap 초기화 코드
+    });
   }, []);
 
   return <Component {...pageProps} />;
