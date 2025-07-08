a51f8f6bb47bef524806ee881bc2c0f66a62324a
GQ <164480701+yongkyu4803@users.noreply.github.com>
2025-07-09 08:17:13 +0900

    fix: move bootstrap initialization to client side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
-import "bootstrap";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    // Bootstrap JS를 동적으로 로드
-    import("bootstrap").then(() => {
-      // Bootstrap 초기화 코드
-    });
+    if (typeof window !== 'undefined') {
+      import("bootstrap").then(() => {
+        // Bootstrap 초기화 코드
+      });
+    }
   }, []);
 
   return <Component {...pageProps} />;
